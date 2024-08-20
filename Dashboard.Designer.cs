1f8e6e013d04b83ae595772768e058008b0a5ce4
Savy_Steven <carlsteven00lacson@Gmail.com>
2024-08-20 22:43:02 +0800

    added dashboard

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace FinanceSys_Test
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            label7 = new Label();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            label6 = new Label();
+            panel2 = new Panel();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            pictureBox8 = new PictureBox();
+            panel6 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            SuspendLayout();
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.SeaGreen;
+            label7.Location = new Point(18, 23);
+            label7.Name = "label7";
+            label7.Size = new Size(99, 21);
+            label7.TabIndex = 33;
+            label7.Text = "Expense List";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(7, 51);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(43, 48);
+            pictureBox6.TabIndex = 22;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(15, 12);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(47, 48);
+            pictureBox7.TabIndex = 15;
+            pictureBox7.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Tahoma", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.SeaGreen;
+            label6.Location = new Point(59, 19);
+            label6.Name = "label6";
+            label6.Size = new Size(443, 35);
+            label6.TabIndex = 15;
+            label6.Text = "Finance Management System";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(244, 206, 20);
+            panel2.Controls.Add(pictureBox7);
+            panel2.Controls.Add(label6);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(64, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(929, 70);
+            panel2.TabIndex = 18;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(7, 466);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(50, 52);
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(10, 200);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(44, 40);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(9, 269);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(44, 44);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(10, 335);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(44, 47);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(9, 119);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(43, 48);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(4, 13, 18);
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(64, 533);
+            panel1.TabIndex = 16;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Controls.Add(pictureBox8);
+            panel3.Controls.Add(label7);
+            panel3.Location = new Point(79, 96);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(267, 156);
+            panel3.TabIndex = 34;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.LightSkyBlue;
+            panel4.Location = new Point(387, 96);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(267, 156);
+            panel4.TabIndex = 35;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.LavenderBlush;
+            panel5.Location = new Point(714, 96);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(267, 156);
+            panel5.TabIndex = 35;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(18, 60);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(44, 44);
+            pictureBox8.TabIndex = 23;
+            pictureBox8.TabStop = false;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.Gray;
+            panel6.Location = new Point(79, 294);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(902, 189);
+            panel6.TabIndex = 36;
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(4, 13, 18);
+            ClientSize = new Size(993, 533);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Dashboard";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label7;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private Label label6;
+        private Panel panel2;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private PictureBox pictureBox8;
+        private Panel panel6;
+    }
+}
